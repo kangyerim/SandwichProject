f9866fe16a52731ec2540f7fee5fe784778d0800
kangyerim <yerimkang521@gmail.com>
2022-06-22 13:07:15 +0900

    css 색상 수정 및 li태그에 data-attribute를 사용하여 선택자 지정할 예정

--- FrontEnd_Mentor_Challenges/[01]Interactive_rating_component/modal.js
+++ FrontEnd_Mentor_Challenges/[01]Interactive_rating_component/modal.js
@@ -2,9 +2,12 @@ const modalForm = document.querySelector('.modal');
 const submitButton = document.querySelector('submit');
 const liTags = document.getElementsByTagName('li');
 
+let clickedLiTags;
+
 function onClick(event) {
   event.preventDefault();
   console.log(event.target.textContent);
+  liTags[event.target.textContent].setAttribute('clicked', true);
 }
 
 function onSubmit(event) {
@@ -15,6 +18,7 @@ function onSubmit(event) {
 function addEventToliTag() {
   for (let i = 0; i < liTags.length; i++) {
     liTags[i].addEventListener('click', onClick);
+    liTags[i].setAttribute('clicked', false);
   }
 }
 
